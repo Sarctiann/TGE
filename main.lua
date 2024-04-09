340da27f01c7320a8c86f620b69b1f3b644a6a32
Sarctiann <sebas.sarc@gmail.com>
2024-04-08 23:29:45 -0300

    no progress yet

--- main.lua
+++ main.lua
@@ -1,1 +1,17 @@
-print("Hello Terminal Game Engine")
+T = require("plterm")
+
+local function render()
+	local prevmode, e, m = T.savemode()
+	if not prevmode then
+		print(prevmode, e, m)
+		os.exit()
+	end
+	T.setrawmode()
+	T.show() -- show cursor
+	T.restoremode(prevmode)
+end --display
+
+local function main()
+	render()
+end
+main()

eef9453f88fa5d1f299dcda2139c218efd6c75a3
Sarctiann <sebas.sarc@gmail.com>
2024-04-18 21:32:53 -0300

    clear last frame before leave

--- examples/tanky.lua
+++ examples/tanky.lua
@@ -65,6 +65,8 @@ console.onData = function(data)
 	if keyData ~= nil then
 		if keyData.key == "ctrl" and keyData.char == "c" then
 			console:write("\x1b[H") -- Clear the screen and returns the prompt to the top
+			cursor.goTo(1, 1)
+			console:write("\n")
 			console:write(cursor.show)
 			console:setMode(0)
 			console:exitMouseMode()

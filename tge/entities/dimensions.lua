3b6423b51858c011dcb3f2951cc1a117073fc56b
Sarctiann (Sebastian Rodriguez) <sebastian.rodriguez@hstpathways.com>
2024-05-14 11:06:27 -0300

    optimized Dimensions

--- tge/entities/dimensions.lua
+++ tge/entities/dimensions.lua
@@ -1,26 +1,28 @@
 local utils = require("tge.utils")
 
---- @class Dimensions
---- @field public width integer
---- @field public height integer
-local Dimensions = {}
+local function new_index(_, key, value)
+	utils:exit_with_error("Attempt to modify Dimensions (%s = %s)", key, value)
+end
+
+local function to_string(self)
+	return string.format("Dimensions: %dx%d", self.width, self.height)
+end
 
 --- @param width integer
 --- @param height integer
 --- @return Dimensions dimensions
-function Dimensions.new(width, height)
+local function new(width, height)
+	--- @class Dimensions
+	--- @field public width integer
+	--- @field public height integer
 	return setmetatable({}, {
 		__index = {
 			width = width,
 			height = height,
 		},
-		__newindex = function(_, key, value)
-			utils:exit_with_error("Attempt Dimensions (%s = %s)", key, value)
-		end,
-		__tostring = function(self)
-			return string.format("Dimensions: %dx%d", self.width, self.height)
-		end,
+		__newindex = new_index,
+		__tostring = to_string,
 	})
 end
 
-return Dimensions
+return { new = new }

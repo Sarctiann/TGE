b6ffd926be26eeb137a97967cfedbd23b48b1993
Sarctiann <sebas.sarc@gmail.com>
2024-05-26 00:55:07 -0300

    improved simple_sprite example

--- examples/simple_sprite.lua
+++ examples/simple_sprite.lua
@@ -22,56 +22,65 @@ local c1 = string.format("%s%s%s", tge.utils.colors.bg(ent.ui.COLOR.Black), "
 local c2 = string.format("%s%s%s", tge.utils.colors.bg(ent.ui.COLOR.Red), " ", tge.utils.colors.resetBg)
 local c3 = string.format("%s%s%s", tge.utils.colors.bg(ent.ui.COLOR.Yellow), " ", tge.utils.colors.resetBg)
 
-local s = Sprite.new({
-	graph = {
-		{ "", cc, "" },
-		{ cc, cc, cc },
-		{ cc, "", cc },
-	},
-	orientation = ORIENTATION.north,
-	options = { lf = 5 },
-}, tge.entities.Boundaries.new(1, 1, game.dimensions.width, game.dimensions.height - 2))
+local function new_tank()
+	local tank = Sprite.new({
+		graph = {
+			{ "", cc, "" },
+			{ cc, cc, cc },
+			{ cc, "", cc },
+		},
+		orientation = ORIENTATION.north,
+		options = { lf = 5 },
+	}, tge.entities.Boundaries.new(1, 1, game.dimensions.width, game.dimensions.height - 2))
 
-s:set_random_graph({
-	graph = {
-		{ c1, c1, "" },
-		{ "", c2, c2 },
-		{ c3, c3, "" },
-	},
-	orientation = ORIENTATION.east,
-})
+	tank:set_random_graph({
+		graph = {
+			{ c1, c1, "" },
+			{ "", c2, c2 },
+			{ c3, c3, "" },
+		},
+		orientation = ORIENTATION.east,
+	})
 
-sprite_seq.spawn(s, { x = 55, y = 20 }, ORIENTATION.north)
+	local rand_x = math.random(1, game.dimensions.width - 6)
+	local rand_y = math.random(1, game.dimensions.height - 7)
+	local rand_orientation = math.random(1, 4)
+	sprite_seq.spawn(tank, { x = rand_x, y = rand_y }, rand_orientation)
+	return tank
+end
 
-local c -- cancel signal var/flag | alternatively this could be initialized as an empty function
+local tank = new_tank()
+local cs -- cancel signal var/flag | alternatively this could be initialized as an empty function
 
 game.on_event = function(e)
 	if e.key == "ctrl" and e.char == "c" then
-		game.exit("Just another step done")
+		game.exit("Not a game yet")
 	elseif e.char == "w" then
-		sprite_seq.move_up_now(s, { oriented = true, cancel_tag = "keep_moving" })
+		sprite_seq.move_up_now(tank, { oriented = true, cancel_tag = "keep_moving" })
 	elseif e.char == "s" then
-		sprite_seq.move_down_now(s, { oriented = true, cancel_tag = "keep_moving" })
+		sprite_seq.move_down_now(tank, { oriented = true, cancel_tag = "keep_moving" })
 	elseif e.char == "a" then
-		sprite_seq.move_left_now(s, { oriented = true, cancel_tag = "keep_moving" })
+		sprite_seq.move_left_now(tank, { oriented = true, cancel_tag = "keep_moving" })
 	elseif e.char == "d" then
-		sprite_seq.move_right_now(s, { oriented = true, cancel_tag = "keep_moving" })
+		sprite_seq.move_right_now(tank, { oriented = true, cancel_tag = "keep_moving" })
 	elseif e.char == "W" then
-		c = sprite_seq.hold_moving_up(s, 5, { cancel_tag = "keep_moving" })
+		cs = sprite_seq.hold_moving_up(tank, 5, { cancel_tag = "keep_moving" })
 	elseif e.char == "A" then
-		c = sprite_seq.hold_moving_left(s, 5, { cancel_tag = "keep_moving" })
+		cs = sprite_seq.hold_moving_left(tank, 5, { cancel_tag = "keep_moving" })
 	elseif e.char == "S" then
-		c = sprite_seq.hold_moving_down(s, 5, { cancel_tag = "keep_moving" })
+		cs = sprite_seq.hold_moving_down(tank, 5, { cancel_tag = "keep_moving" })
 	elseif e.char == "D" then
-		c = sprite_seq.hold_moving_right(s, 5, { cancel_tag = "keep_moving" })
-	elseif e.char == "x" and c then -- here we are checking that `c` has been initialized with the cleaner function
-		c()
+		cs = sprite_seq.hold_moving_right(tank, 5, { cancel_tag = "keep_moving" })
+	elseif e.char == "x" and cs then -- here we are checking that `c` has been initialized with the cleaner function
+		cs()
 	elseif e.char == "X" then
-		sprite_seq.delete(s, "keep_moving")
+		sprite_seq.delete(tank, "keep_moving")
+	elseif e.char == "n" and not tank.is_present then
+		tank = new_tank()
 	elseif e.event and (e.event == "press" or e.event == "hold") then
 		local x, y = e.x, e.y
 		if x > 2 and x <= game.dimensions.width - 2 and y > 1 and y <= game.dimensions.height - 3 then
-			sprite_seq.translate(s, x - 2, y - 1)
+			sprite_seq.translate(tank, x - 2, y - 1)
 		end
 	end
 end
